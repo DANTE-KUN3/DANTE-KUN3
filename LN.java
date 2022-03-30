0ab5b9dbbd89ca089d054e12eb5d0ff48f51f8ce
BISHAL <74647844+DANTE-KUN3@users.noreply.github.com>
2022-03-30 18:08:11 +0530

    Add files via upload

---
+++ LN.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class LN{
+ public static void main(String args[]){
+ int array[] = {10, 20, 25, 63, 96, 57};
+ int size = array.length;
+ Arrays.sort(array);
+ System.out.println("sorted Array ::"+Arrays.toString(array));
+ int res = array[size-1];
+ System.out.println("largest element is ::"+res);
+ }
+}
